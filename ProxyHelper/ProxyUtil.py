dd9779f9dd92da43366a5eb09edf7ebddc43e29a
9DemonFox <1196752479@qq.com>
2020-03-22 10:54:32 +0800

    第2次提交，增加所有文件

---
+++ ProxyHelper/ProxyUtil.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     ProxyHelper
+   Description : 修改检查代理合格的方式
+   Author :        JHao
+   date：          2020-3-22 01:11:58
+-------------------------------------------------
+   Change Activity:
+                   2019/8/8:
+-------------------------------------------------
+"""
+from Util import validUsefulProxy
+from datetime import datetime
+
+
+
+
+def checkProxyUseful(proxy_obj):
+    """
+    检测代理是否可用
+    :param proxy_obj: Proxy object
+    :return: Proxy object, status
+    """
+
+    if validUsefulProxy(proxy_obj.proxy):
+        # 检测通过 更新proxy属性
+        proxy_obj.check_count += 1
+        proxy_obj.last_status = 1
+        proxy_obj.last_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if proxy_obj.fail_count > 0:
+            proxy_obj.fail_count -= 1
+        return proxy_obj, True
+    else:
+        proxy_obj.check_count += 1
+        proxy_obj.last_status = 0
+        proxy_obj.last_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        proxy_obj.fail_count += 1
+        return proxy_obj, False
